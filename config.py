98fcfcea3cd57629d40fe39c7ca75a72d6d1ecd0
Zhang Elisa <ez018@hdm-stuttgart.de>
2023-05-23 14:54:49 +0200

    added some folders

--- config.py
+++
@@ -1,6 +0,0 @@
-
-mqtt_url = "a3uf1j30lr99lx-ats.iot.eu-central-1.amazonaws.com"
-root_ca = 'aws_connect_test\\root-CA.crt'
-public_crt = 'aws_connect_test\\Anjo_Laptop.cert.pem'
-private_key = 'aws_connect_test\\Anjo_Laptop.private.key'
-COM_port = 'COM3'
