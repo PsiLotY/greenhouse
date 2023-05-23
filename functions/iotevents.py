94b59486dc86a68c9b3cd6a54496568f45d3161e
Zhang Elisa <ez018@hdm-stuttgart.de>
2023-05-23 17:09:06 +0200

    iotEvents createDetectorModels

--- functions/iotevents.py
+++ functions/iotevents.py
@@ -1,2 +1,11 @@
 import boto3
 
+def create_detector_model(event, context):
+    client = boto3.client('iotevents')
+
+    response = client.create_detector_model(
+        detectorModelName = event['detectorModelName'],
+        detectorModelDefinition = event['detectorModelDefinition'],
+        roleArn = event['roleArn']
+    )
+
