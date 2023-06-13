445d31ea7e8e39410e3f947441070feacb5d1027
Anjo <aw181@hdm-stuttart.de>
2023-06-13 15:26:50 +0200

    create thing from top-level main

--- terraform/Thing/main.tf
+++ terraform/Thing/main.tf
@@ -1,33 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
-    }
-  }
-  
-  required_version = ">= 1.2.0"
-}
-
-provider "aws" {
-  region = "eu-central-1"  # Update with your desired region
-  access_key = "AKIAWAWJBWF65CWABPGM"
-  secret_key = "nkMDWAVUsGBa1QKsU/GrrHxY+YqtukrBh8Oqzzx/"
-}
-
 resource "aws_iot_thing" "my_thing" {
-  name = "terra_thing"
-  attributes = {
-    arn = "arn:aws:iam::aws:policy/service-role/AWSIoTThingsRegistration"
-  }
+  name = var.thing_name
+  attributes = var.thing_attributes
 }
 
 resource "aws_iot_policy" "my_policy" {
-  name   = "terra_policy"
+  name   = "my_terra_policy"
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -55,7 +32,7 @@ resource "tls_private_key" "my_private_key" {
 resource "tls_self_signed_cert" "self_signed" {
   private_key_pem       = tls_private_key.my_private_key.private_key_pem
   subject {
-    common_name         = "terra_thing"
+    common_name         = var.thing_name
   }
   
   validity_period_hours = 8760  # 1 year
@@ -75,14 +52,5 @@ resource "aws_iot_policy_attachment" "my_policy_attachment" {
 resource "aws_iot_thing_principal_attachment" "my_principal_attachment" {
   thing = aws_iot_thing.my_thing.name
   principal  = aws_iot_certificate.my_certificate.arn
-}
-
-output "cert" {
-  value = tls_self_signed_cert.self_signed.cert_pem
-}
-
-output "key" {
-  value     = tls_private_key.my_private_key.private_key_pem
-  sensitive = true
 }
 
---
+++ terraform/Thing/variables.tf
@@ -0,0 +1,20 @@
+
+
+
+
+
+#gpt
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "thing_name" {
+  description = "Name of the IoT thing"
+  type        = string
+}
+
+variable "thing_attributes" {
+  description = "Attributes of the IoT thing"
+  type        = map(any)
+}
