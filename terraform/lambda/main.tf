db446b285d470c05709566e370925123d53ca969
Zhang Elisa <ez018@hdm-stuttgart.de>
2023-06-13 19:10:36 +0200

    comment

--- terraform/lambda/main.tf
+++ terraform/lambda/main.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "example" {
   function_name    = "example-lambda"
   filename         = "test.py"
-  source_code_hash = filebase64sha256("../functions/test.py")
+  source_code_hash = filebase64sha256("../functions/test.py") #path of the file are made from the main.tf where the module is called
   handler          = "index.handler"
   role             = "arn:aws:iam::413812240765:role/service-role/test-role-dmwgeyo6"
   runtime          = "python3.7"
