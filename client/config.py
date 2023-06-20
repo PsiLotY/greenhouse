29e29195c44818e3da9015add88a088d0e16c416
Johannes Rödel <jr125@hdm-stuttgart.de>
2023-06-20 16:31:26 +0200

    made the client clearer

--- client/config.py
+++ client/config.py
@@ -1,6 +1,5 @@
-
 mqtt_url = "a3uf1j30lr99lx-ats.iot.eu-central-1.amazonaws.com"
-root_ca = 'auth\\root.pem'
-public_crt = 'auth\\certificate.pem.crt'
-private_key = 'auth\\private.pem.key'
-COM_port = 'COM9'
+root_ca = 'certs/root-CA.crt'
+public_crt = 'certs/dings.cert.pem'
+private_key = 'certs/dings.private.key'
+COM_port = 'COM3'
