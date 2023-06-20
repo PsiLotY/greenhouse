7c3eeebd0864fdd5ede6b27706b144e9f8ae9039
Anjo <aw181@hdm-stuttart.de>
2023-06-20 19:41:31 +0200

    refactors code to utilize functions

--- client/main.py
+++ client/main.py
@@ -1,5 +1,3 @@
-import paho.mqtt.client as mqtt
-import ssl
 from time import sleep
 import json
 import config
@@ -7,22 +5,14 @@ from iotee import Iotee
 import signal
 import sys
 import time 
+from utils import connect_to_mqtt, subscribe_to, start_iotee
 
 #handles shutting down threads on ctrl+c
-def signal_handler(signal, frame):
+def signal_handler(signal, frame, iotee):
     print('Shutting down')
     iotee.stop()
     sys.exit(0)
     
-signal.signal(signal.SIGINT, signal_handler)
-
-#gets data from config.py
-mqtt_url = config.mqtt_url
-root_ca = config.root_ca
-public_crt = config.public_crt  
-private_key = config.private_key
-COM_port = config.COM_port
-
 #define callback functions for mqtt
 connflag = False
 def on_connect(client, userdata, flags, response_code):
@@ -37,38 +27,20 @@ def on_message(client, userdata, msg):
     print("Message received, topic: ", msg.topic)
     print("Message payload: ", msg.payload.decode())
 
-#setup mqtt client
-client = mqtt.Client()
-client.tls_set(root_ca,
-                certfile = public_crt,
-                keyfile = private_key,
-                cert_reqs = ssl.CERT_REQUIRED,
-                tls_version = ssl.PROTOCOL_TLSv1_2,
-                ciphers = None)
-client.on_connect = on_connect
-client.on_publish = on_publish
-client.on_message = on_message
 
 # message template
-message = """{
-                            "timestamp": 0,
-                            "inputName": "sensorData",
-                            "messageId": "555e8fef-6c80-48f3-a6b5-2d2160d472f5",
-                            "pressure": 0,
-                            "temperature": 0,
-                            "humidity": 0,
-                            "light": 0,
-                            "proximity": 0
-                        
-            }"""
+message = {
+            "timestamp": 0,
+            "inputName": "sensorData",
+            "messageId": "555e8fef-6c80-48f3-a6b5-2d2160d472f5",
+            "pressure": 0,
+            "temperature": 0,
+            "humidity": 0,
+            "light": 0,
+            "proximity": 0
+            }
 
-data = json.loads(message)
-
-print(data)
-
-# setup iotee
-iotee = Iotee(COM_port)
-iotee.start()
+data = message
 
 
 # callback functions for iotee
@@ -92,42 +64,43 @@ def on_proximity(value):
     print("proximity: {:.2f}".format(value))
 
 
-iotee.on_temperature = on_temperature
-iotee.on_humidity = on_humidity
-iotee.on_light = on_light
-iotee.on_proximity = on_proximity
 
 #gets the sensor data from the connected devive on COM_port through callback functions
-def request_sensor_data(timestamp):
+def request_sensor_data(iotee):
+    timestamp = int(time.time())
+    print('\n')
+    print('time of getting data:',timestamp)
+    data['timestamp'] = timestamp
     iotee.request_temperature()
     iotee.request_humidity()
     iotee.request_light()
     iotee.request_proximity()
-    print(timestamp)
-    data['timestamp'] = timestamp
-    print('\n')
 
-print ('Connecting to AWS IoT Broker...')
-client.connect(mqtt_url, port = 8883, keepalive=60)
-client.loop_start()
-client.subscribe('message_test')
+
 
 #main loop for sending data
 def main():
+    iotee = start_iotee(config.COM_port)
+    signal.signal(signal.SIGINT, lambda signal, frame: signal_handler(signal, frame, iotee))
+
+    iotee.on_temperature = on_temperature
+    iotee.on_humidity = on_humidity
+    iotee.on_light = on_light
+    iotee.on_proximity = on_proximity
+
+
+    client = connect_to_mqtt()
+    client.on_connect = on_connect
+    client.on_publish = on_publish
+    client.on_message = on_message
+
+    subscribe_to(client, ['message_test'], 1)    
     while(True):
-        timestamp = int(time.time())
-        print (timestamp)
-        request_sensor_data(timestamp)
+        request_sensor_data(iotee)
         
         sleep(1)
-        if connflag == True:
-            print ('Publishing...')
-            client.publish('hannes_test', payload=json.dumps(data), qos=1)
+        client.publish('hannes_test', payload=json.dumps(data), qos=1)
         
-        else:
-            client.connect(mqtt_url, port = 8883, keepalive=60)
-            client.loop_start()
-            print ('waiting for connection...')
         sleep(4)
 
 if __name__ == '__main__':
---
+++ client/utils.py
@@ -0,0 +1,64 @@
+import paho.mqtt.client as mqtt
+import config
+import ssl
+from iotee import Iotee
+
+def connect_to_mqtt():
+    '''
+    Connects to a mqtt broker with tls encryption 
+    params: 
+        None
+    return: 
+        client: mqtt client object
+    '''
+    mqtt_url = config.mqtt_url
+    client = mqtt.Client()
+    set_tls(client)
+    print ('Connecting to AWS IoT Broker...')
+    client.connect(mqtt_url, port = 8883, keepalive=60)
+    return client
+
+def set_tls(client: mqtt.Client):
+    '''
+    Sets the tls encryption for the mqtt client
+    params: 
+        client: mqtt client object
+    return: 
+        None
+    '''
+    root_ca = config.root_ca
+    public_crt = config.public_crt  
+    private_key = config.private_key
+    client.tls_set(root_ca,
+                        certfile = public_crt,
+                        keyfile = private_key,
+                        cert_reqs = ssl.CERT_REQUIRED,
+                        tls_version = ssl.PROTOCOL_TLSv1_2,
+                        ciphers = None)
+    
+
+def subscribe_to(client: mqtt.Client, topics: list, qos: int):
+    '''
+    Subscribes to a list of topics 
+    params: 
+        client: mqtt client object
+        topics: list of topics to subscribe to
+        qos: quality of service
+    return: 
+        None
+    '''
+    for topic in topics:
+        client.subscribe(topic, qos=qos)
+
+
+def start_iotee(com_port: str):
+    '''
+    Starts a thread for the iotee device 
+    params: 
+        com_port: com port where the iotee device is connected to
+    return: 
+        iotee: iotee object
+    '''
+    iotee = Iotee(config.COM_port)
+    iotee.start()
+    return iotee
