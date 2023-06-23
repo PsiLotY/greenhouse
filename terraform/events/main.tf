e085d726217bea06c17ef5eb27d48eb6ae5419c2
Anjo <aw181@hdm-stuttart.de>
2023-06-23 14:01:01 +0200

    corrects window events and adds sprinkler events

--- terraform/events/main.tf
+++ terraform/events/main.tf
@@ -1,47 +1,47 @@
-resource "awscc_iotevents_input" "test_input" {
+resource "awscc_iotevents_input" "device_input" {
   input_definition = {
     attributes = [{
       json_path = "messageId"
       }, {
-      json_path = "sensor_data.humidity"
+      json_path = "humidity"
       }, {
-      json_path = "sensor_data.temperature"
+      json_path = "temperature"
       }, {
-      json_path = "sensor_data.light"
+      json_path = "light"
     }]
   }
   input_name = "device_input"
 }
 
 resource "awscc_iotevents_detector_model" "window" {
-  detector_model_name        = "window_events"
+  detector_model_name        = "monitors and changes the state of the windows"
   detector_model_description = "window events"
-  evaluation_method          = "BATCH"
-  role_arn                   = "arn:aws:iam::413812240765:role/service-role/Model"
+  evaluation_method          = "SERIAL"
+  role_arn                   = "arn:aws:iam::413812240765:role/service-role/IoTCoreRole"
 
   detector_model_definition = {
     states = [
       {
-        state_name = "window_closed"
+        state_name = "windows_closed"
         on_input = {
           events = []
           transition_events = [
             {
-              event_name = "open_window"
-              condition  = "$input.device_input.sensor_data.temperature > 38"
+              event_name = "open_windows"
+              condition  = "$input.device_input.temperature > 38"
               actions    = []
-              next_state = "window_open"
+              next_state = "windows_open"
             }
           ]
         }
         on_enter = {
           events = [{
-            event_name = "enter_close_window_state",
+            event_name = "enter_windows_closed_state",
             condition = "true",
             actions = [
               {
                 iot_topic_publish = {
-                  mqtt_topic = "events/window"
+                  mqtt_topic = "iot/actor_data"
                 }
               }
             ]
@@ -52,26 +52,26 @@ resource "awscc_iotevents_detector_model" "window" {
         }
       },
       {
-        state_name = "window_open"
+        state_name = "windows_open"
         on_input = {
           events = []
           transition_events = [
             {
-              event_name = "close_window"
-              condition  = "$input.device_input.sensor_data.temperature < 30"
+              event_name = "close_windows"
+              condition  = "$input.device_input.temperature < 30"
               actions    = []
-              next_state = "window_closed"
+              next_state = "windows_closed"
             }
           ]
         }
         on_enter = {
           events = [{
-            event_name = "enter_close_window_state",
+            event_name = "enter_windows_open_state",
             condition = "true",
             actions = [
               {
                 iot_topic_publish = {
-                  mqtt_topic = "events/window"
+                  mqtt_topic = "iot/actor_data"
                 }
               }
             ]
@@ -82,7 +82,80 @@ resource "awscc_iotevents_detector_model" "window" {
         }
       }
     ]
-    initial_state_name = "window_closed"
+    initial_state_name = "windows_closed"
   }
 }
 
+
+resource "awscc_iotevents_detector_model" "sprinkler" {
+  detector_model_name        = "Starts and stops the sprinklers based on humidity"
+  detector_model_description = "sprinkler events"
+  evaluation_method          = "SERIAL"
+  role_arn                   = "arn:aws:iam::413812240765:role/service-role/IoTCoreRole"
+
+  detector_model_definition = {
+    states = [
+      {
+        state_name = "sprinklers_off"
+        on_input = {
+          events = []
+          transition_events = [
+            {
+              event_name = "start_sprinklers"
+              condition  = "$input.device_input.humidity < 20"
+              actions    = []
+              next_state = "sprinklers_on"
+            }
+          ]
+        }
+        on_enter = {
+          events = [{
+            event_name = "enter_sprinklers_off_state",
+            condition = "true",
+            actions = [
+              {
+                iot_topic_publish = {
+                  mqtt_topic = "iot/actor_data"
+                }
+              }
+            ]
+          }]
+        }
+        on_exit = {
+          events = []
+        }
+      },
+      {
+        state_name = "sprinklers_on"
+        on_input = {
+          events = []
+          transition_events = [
+            {
+              event_name = "stop_sprinklers"
+              condition  = "$input.device_input.humidity >= 20"
+              actions    = []
+              next_state = "sprinklers_off"
+            }
+          ]
+        }
+        on_enter = {
+          events = [{
+            event_name = "enter_sprinklers_on_state",
+            condition = "true",
+            actions = [
+              {
+                iot_topic_publish = {
+                  mqtt_topic = "iot/actor_data"
+                }
+              }
+            ]
+          }]
+        }
+        on_exit = {
+          events = []
+        }
+      }
+    ]
+    initial_state_name = "sprinklers_off"
+  }
+}
