a66c76ab01d43258acc494218ff7c72c33444653
Zhang Elisa <ez018@hdm-stuttgart.de>
2023-06-23 16:33:30 +0200

    fixed filename, lambda works now

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,14 +1,14 @@
-# data "archive_file" "lambda" {
-#     type        = "zip"
-#     source_file  = "../lambda_functions/TestLambda.py"
-#     output_path = "lambda_function_payload.zip"
-#  }
+data "archive_file" "lambda" {
+    type        = "zip"
+    source_file  = "../lambda_functions/TestLambda.py"
+    output_path = "lambda_function_payload.zip"
+ }
 
-# resource "aws_lambda_function" "example" {
-#     function_name    = "test_lambda"
-#     filename         = "../lambda_functions/TestLambda.py"
-#     handler          = "lambda_handler"
-#     role             = aws_iam_role.core_role.arn
-#     source_code_hash = data.archive_file.lambda.output_base64sha256
-#     runtime          = "python3.7"
-# }
+resource "aws_lambda_function" "example" {
+    function_name    = "test_lambda"
+    filename         = "lambda_function_payload.zip"
+    handler          = "lambda_handler"
+    role             = aws_iam_role.core_role.arn
+    source_code_hash = data.archive_file.lambda.output_base64sha256
+    runtime          = "python3.7"
+}
