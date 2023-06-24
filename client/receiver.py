ffb8a3ca968832b4f3a48802b58f11c6abae5aa9
Anjo <aw181@hdm-stuttart.de>
2023-06-24 19:10:19 +0200

    adds button events, adds display&led to receiver

--- client/receiver.py
+++ client/receiver.py
@@ -1,6 +1,15 @@
 import json
-from utils import connect_to_mqtt, subscribe_to
+from utils import connect_to_mqtt, subscribe_to, start_iotee
+import config
+import sys
+import signal
 
+
+#handles shutting down threads on ctrl+c
+def signal_handler(signal, frame, iotee):
+    print('Shutting down')
+    iotee.stop()
+    sys.exit(0)
 
 #define callback functions for mqtt
 def on_connect(client, userdata, flags, response_code):
@@ -11,12 +20,73 @@ def on_subscribe(client, userdata, msg):
     print(msg.payload)
 
 def on_message(client, userdata, msg):
-    print("Message received, topic: ", msg.topic)
-    message = json.dumps(msg.payload.decode())
-    print("Message payload: ", message)
+    global text
+    topic = msg.topic
+    message = json.loads(msg.payload.decode())
+    # print(message)
+    if message['state'] == 'sprinklers_on':
+        iotee.set_led(255, 0, 0) #red
+        display_text = 'Sprinklers \nare on'
+        text = process_text(display_text)
+        iotee.set_display(text)
+        print('Sprinklers are on')
+
+    elif message['state'] == 'sprinklers_off':
+        iotee.set_led(0, 255, 0) #green
+        display_text = 'Sprinklers \nare off'
+        text = process_text(display_text)
+        iotee.set_display(text)
+        print('Sprinklers are off')
+
+    elif message['state'] == 'windows_closed':
+        iotee.set_led(0, 0, 255) #blue
+        display_text = 'Windows \nare closed'
+        text = process_text(display_text)
+        iotee.set_display(text)
+        print('Windows are closed')
+
+    elif message['state'] == 'windows_open':
+        iotee.set_led(255, 255, 0) #yellow
+        display_text = 'Windows \nare open'
+        text = process_text(display_text)
+        iotee.set_display(text)
+        print('Windows are open')
+
+    elif message['state'] == 'lights_on':
+        iotee.set_led(0, 255, 255) #cyan
+        display_text = 'Lights \nare on'
+        text = process_text(display_text)
+        iotee.set_display(text)
+        print('Lights are on')
+
+    elif message['state'] == 'lights_off':
+        iotee.set_led(255, 0, 255) #purple
+        display_text = 'Lights \nare off'
+        text = process_text(display_text)
+        iotee.set_display(text)
+        print('Lights are off')
+
+
+def process_text(text):
+    global old_texts
+    old_texts.append(text)
+    # only keep the 3 newest appended texts if bigger than 3
+    if len(old_texts) > 3:
+        old_texts = old_texts[-3:]
+    # create a new text of 3 elements from old_texts, seperated by \n
+    new_text = ''
+    for old_text in old_texts:
+        new_text += f"{old_text}\n"
+    return new_text
+
+
+old_texts = []
+iotee = start_iotee(config.COM_port)
 
 #main loop for receiving data
 def main():
+    signal.signal(signal.SIGINT, lambda signal, frame: signal_handler(signal, frame, iotee))
+    
     client = connect_to_mqtt()
     
     client.on_connect = on_connect
